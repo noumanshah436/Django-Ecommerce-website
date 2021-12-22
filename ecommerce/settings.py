a1b68607da23a9451956d51d9fb887b011a8fc8b
NoumanShah042 <noumanrehman042@gmail.com>
2021-12-22 23:25:31 +0500

    category page added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -113,15 +113,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = "static/"
+
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 
 def home(request):
-    return render(request , 'store/index.html');
+    return render(request, 'store/index.html')
+
+
+def collections(request):
+    categories = Category.objects.filter(status=0)
+    context = {'categories': categories}
+    return render(request, 'store/collections.html', context)
